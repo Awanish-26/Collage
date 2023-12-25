40fca492c86bcb051cb73baab7555b1bd6d48f0c
Awanish-26 <awanishyadav996@gmail.com>
2023-12-26 00:07:45 +0530

    .

---
+++ 3rd-sem/queue_using_ll.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void enqueue();
+void dequeue();
+void front();
+void rear();
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} S;
+
+int main()
+{
+    return 0;
+}
